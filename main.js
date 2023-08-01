ccc2a05a34e29deef21ed13c4621d720012490f8
Alan Lujan <al.lujan20@gmail.com>
2023-07-31 23:51:15 -0300

    segunda pre-entrega

--- main.js
+++ main.js
@@ -1,44 +1,38 @@
-//SIMULADOR PARA CALCULAR A QUÉ PAQUETE BANCARIO PUEDE ACCEDER UN CLIENTE SEGUN SU SUELDO.
 
-let nombre = prompt("Por favor, ingrese su nombre:")
-alert("¡Bienvenidx a nuestro simulador de paquetes bancarios, " + nombre + "! Comencemos.")
 
-function calculoPaquete() {
-    const sueldo = parseInt(prompt("Por favor, ingrese su sueldo"))
-   
-    if(sueldo >= 195000){
-        alert ("Usted puede adquirir el paquete de: Infinity Black")
+const Pelicula = function (nombre, dia, precio, entradasDisponibles) {
+  this.nombre = nombre;
+  this.dia = dia;
+  this.precio = precio;
+  this.entradasDisponibles = entradasDisponibles;
+};
 
-    }else if(sueldo >= 135000 && sueldo < 195000 ){
-        alert ("Usted puede adquirir el paquete de: Infinity Platinum")
+let pelicula1 = new Pelicula("oppenheimer","miercoles, viernes y sabado", 3500, 95);
+let pelicula2 = new Pelicula("barbie", "martes, viernes y domingo", 3500, 89);
+let pelicula3 = new Pelicula("mision imposible", "lunes y jueves", 3500, 103);
+let pelicula4 = new Pelicula("indiana jones", "sabado y domingo", 3500, 133);
 
-    }else if(sueldo >= 70000 && sueldo < 135000 ){
-        alert ("Usted puede adquirir el paquete de: Infinity Gold")
-        
-    }else if(sueldo >= 60000 && sueldo < 70000 ){
-        alert ("Usted puede adquirir el paquete de: Infinity 1")
-        
-    }else if(sueldo >= 40000 && sueldo < 60000 ){
-        alert ("Usted puede adquirir el paquete de: Super Cuenta 3")
-        
-    }else if(sueldo >= 5000 && sueldo < 40000){
-        alert ("Usted puede adquirir el paquete de: Super Cuenta")
-        
+let cartelera = [pelicula1, pelicula2, pelicula3, pelicula4];
 
-    }else{
-        alert("No cumple con los requerimientos mínimos para adquirir un paquete bancario")
-    }
-  
+function filtrarPelicula() {
+  let busqueda = prompt("Ingrese la película que desea ver");
+  let resultado = cartelera.filter((pelicula) =>
+    pelicula.nombre.includes(busqueda)
+  );
+
+  if (resultado.length > 0) {
+    console.table(resultado);
+  } else {
+    alert("no existe coincidencia con: " + busqueda);
+  }
 }
-
 
 //Ciclo
 
-do{
-    calculoPaquete();
-} while(confirm("¿Desea simular otro sueldo?"));
+do {
+    filtrarPelicula();
+} while (confirm("¿Desea buscar otra pelicula?"));
 
-    alert("Gracias por utilizar nuestro simulador, " + nombre + ". ¡Que tenga buen día!")
-
-
-
+alert(
+  "Gracias por elegirnos. ¡Que tenga buen día!"
+);
